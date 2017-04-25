7df3dbd854c3cf8d3f0a507768972fe7dd799343
Denner Evaldt <dnrevaldt@gmail.com>
2017-04-25 01:51:41 -0300

    db

--- www/js/app.config.js
+++ www/js/app.config.js
@@ -13,45 +13,42 @@ function run($rootScope, $location, $http, LoginService, HomeService, ContactSer
           ContactService.setContacts(allContacts);
         });
 
-        // Obtém objeto do BD
-        var db = $cordovaSQLite.openDB({name: "socialNetworkApp.db"});
-        DBService.reset(db); // reseta dados e tabelas
-        DBService.init(db); // inicializa dados e tabelas
+      // Obtém objeto do BD
+      var db = $cordovaSQLite.openDB({name: "socialNetworkApp.db"});
+      DBService.reset(db); // reseta dados e tabelas
+      DBService.init(db); // inicializa dados e tabelas
 
-        // populate publications
-        $http.get('appdata/publications.json')
+      // populate publications
+      $http.get('appdata/publications.json')
+        .then(function(response){
+          HomeService.populatePublications(response.data);
+        });
+
+        // populate friends
+        $http.get('appdata/friends.json')
           .then(function(response){
-            console.log('PASSOU PELO PUB >>>');
-            HomeService.populatePublications(response.data);
+            HomeService.populateFriends(response.data);
           });
 
-          // populate friends
-          $http.get('appdata/friends.json')
-            .then(function(response){
-              console.log('PASSOU PELO FRIENDS >>>');
-              HomeService.populateFriends(response.data);
-            });
-
-          // populate users
-          $http.get('appdata/users.json')
-            .then(function(response){
-              console.log('PASSOU PELO POPULATE >>>');
-              LoginService.populateUsers(response.data);
-            });
-
-          // check authenticate user start change route
-          $rootScope.$on('$stateChangeStart', function (event,next,current) {
-            var localUser = localStorage.getItem('socialCookieUni') || undefined;
-            if (!localUser) {
-              $location.path('login');
-            }
+        // populate users
+        $http.get('appdata/users.json')
+          .then(function(response){
+            LoginService.populateUsers(response.data);
           });
 
-          /*
-          Receive emitted message and broadcast it.
-          */
-          $rootScope.$on('handleEmit', function (event, args) {
-            $rootScope.$broadcast('handleBroadcast', args);
-          });
+        // check authenticate user start change route
+        $rootScope.$on('$stateChangeStart', function (event,next,current) {
+          var localUser = localStorage.getItem('socialCookieUni') || undefined;
+          if (!localUser) {
+            $location.path('login');
+          }
+        });
+
+        /*
+        Receive emitted message and broadcast it.
+        */
+        $rootScope.$on('handleEmit', function (event, args) {
+          $rootScope.$broadcast('handleBroadcast', args);
+        });
     });
 }
